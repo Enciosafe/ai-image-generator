75e3e0e449371e229904934a9bda8f8eba1fc2e7
Stas <finneganlucky@gmail.com>
2023-05-24 14:26:42 +0200

    minor  [header + sprompt ui]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
+import PromptInput from '@/components/PromptInput'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Header/>
+      <Header />
+      <PromptInput />
       <body className={inter.className}>{children}</body>
     </html>
   )
