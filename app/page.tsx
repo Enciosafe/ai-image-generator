01d3da10c0f6dfbecf459afb2561b56961e048d0
Stas <finneganlucky@gmail.com>
2023-05-29 17:04:01 +0200

    openai api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className='text-red-500'>Hello bitch</h1>
+      <h1 className=''>Hello bitch</h1>
     </main>
   )
 }
--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,10 +1,17 @@
 'use client'
+import fetchSuggestionFromChatGPT from '@/lib/fetchSuggestionFromChatGPT'
 import React, { useState } from 'react'
+import useSWR from 'swr'
 
-type Props = {}
+type Props = {
+}
 
 const PromptInput = (props: Props) => {
     const [input, setInput] = useState('')
+
+    const { data: suggestion, isLoading, mutate, isValidating } = useSWR('/api/suggestion', fetchSuggestionFromChatGPT, {
+        revalidateOnFocus: false,
+    })
     
   return (
       <div className="m-10">
